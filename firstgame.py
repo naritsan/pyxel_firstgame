5bb293f6c9c728c0104a5401bfe8bcc87b12819e
nekoou <naritsan3@gmail.com>
2025-11-01 23:38:56 +0900

    firstgameを上下左右移動対応

--- firstgame.py
+++ firstgame.py
@@ -4,13 +4,21 @@ class App:
     def __init__(self):
         pyxel.init(160, 120)
         self.x = 0
+        self.y = 0
         pyxel.run(self.update, self.draw)
 
     def update(self):
-        self.x = (self.x + 1) % pyxel.width
+        if pyxel.btn(pyxel.KEY_LEFT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_LEFT):
+            self.x -= 1
+        elif pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_RIGHT):
+            self.x += 1
+        elif pyxel.btn(pyxel.KEY_UP) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_UP):
+            self.y -= 1
+        elif pyxel.btn(pyxel.KEY_DOWN) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_DOWN):
+            self.y += 1
 
     def draw(self):
-        pyxel.cls(0)
-        pyxel.rect(self.x, 0, 8, 8, 9)
+        pyxel.cls(7)
+        pyxel.rect(self.x, self.y, 8, 8, 9)
 
 App()
