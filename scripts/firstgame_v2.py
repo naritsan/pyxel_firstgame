858dff7c47fe7ac7c1b8be72695aca9da1fe51d3
nekoou <naritsan3@gmail.com>
2025-11-02 21:30:46 +0900

    当たり判定を追加

--- scripts/firstgame_v2.py
+++ scripts/firstgame_v2.py
@@ -8,7 +8,8 @@ class App:
         self.x = 0
         self.y = 0
         self.apple_x = 20
-        self.apple_y = 15 
+        self.apple_y = 15
+        self.apple_eaten = False
 
         pyxel.run(self.update, self.draw)
 
@@ -22,9 +23,13 @@ class App:
         elif pyxel.btn(pyxel.KEY_DOWN) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_DOWN):
             self.y += 1
 
+        if self.x == self.apple_x and self.y == self.apple_y:
+            self.apple_eaten = True
+
     def draw(self):
         pyxel.cls(6)
         pyxel.pset(self.x, self.y,1)
-        pyxel.pset(self.apple_x, self.apple_y,8)
+        if not self.apple_eaten:
+            pyxel.pset(self.apple_x, self.apple_y,8)
 
 App()
