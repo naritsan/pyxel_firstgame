a0d319a9cec63f0ee8fc45c4ad5055834af7addc
nekoou <naritsan3@gmail.com>
2025-11-03 01:00:17 +0900

    タイマー、スコア表示を追加

--- scripts/firstgame_v2.py
+++ scripts/firstgame_v2.py
@@ -6,17 +6,22 @@ class App:
             40, 30, title="secondgame!", fps=20, display_scale=20
         )
         self.x = 30
-        self.y = 0
+        self.y = 10
         self.apple_x = pyxel.rndi(19, pyxel.width - 1)
         self.apple_y = pyxel.rndi(9, pyxel.height - 1)
         self.rotten_apple_x = pyxel.rndi(19, pyxel.width - 1)
         self.rotten_apple_y = pyxel.rndi(9, pyxel.height - 1)
         self.score = 0
         self.game_over = False
+        self.time_up = False
+        self.timer = 10
 
         pyxel.run(self.update, self.draw)
 
     def update(self):
+        if self.game_over or self.time_up:
+            return
+
         if pyxel.btn(pyxel.KEY_LEFT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_LEFT):
             self.x -= 1
         elif pyxel.btn(pyxel.KEY_RIGHT) or pyxel.btn(pyxel.GAMEPAD1_BUTTON_DPAD_RIGHT):
@@ -31,19 +36,31 @@ class App:
             self.score += 100
             self.apple_x = pyxel.rndi(19, pyxel.width - 1)
             self.apple_y = pyxel.rndi(9, pyxel.height - 1)
+            self.rotten_apple_x = pyxel.rndi(19, pyxel.width - 1)
+            self.rotten_apple_y = pyxel.rndi(9, pyxel.height - 1)
         
         if self.x == self.rotten_apple_x and self.y == self.rotten_apple_y:
             self.game_over = True
+        
+        if pyxel.frame_count != 0 and pyxel.frame_count % 20 == 0 and not self.game_over:
+            self.timer -= 1
+            if self.timer <= 0:
+                self.time_up = True
 
     def draw(self):
         if self.game_over:
             pyxel.cls(0)
             pyxel.text(4, 11, "GAMEOVER", 8)
+        elif self.time_up:
+            pyxel.cls(0)
+            pyxel.text(0, 11, f"SCORE:{self.score} ", 7)
         else:
             pyxel.cls(6)
             pyxel.pset(self.rotten_apple_x, self.rotten_apple_y, 3)
             pyxel.pset(self.apple_x, self.apple_y,8)
-            pyxel.rect(0, 0, 18, 8, 0)
+            pyxel.rect(0, 0, 40, 8, 0)
+            pyxel.rect(30, 0, 10, 8, 14)
+            pyxel.text(32,1,f"{self.timer}",7)
             pyxel.text(1, 1, f"{self.score}", 7)
             pyxel.pset(self.x, self.y,1)
 
